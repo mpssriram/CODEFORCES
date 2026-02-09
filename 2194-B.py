dc7c0b62873ef5f1d587c3c68ab8a0f3c23589a7
mpssriram <msriram.0517@gmail.com>
2026-02-10 01:03:19 +0530

    --

--- 2194-B.py
+++ 2194-B.py
@@ -2,23 +2,12 @@ t = int(input())
 for _ in range(t):
     n,x,y = map(int,input().split())
     arr = list(map(int,input().split()))
-    summed = sum(arr)
-    print(arr)
-    lost = x-y
     moves = 0
-    remainder = []
-    for i in range(n-1):
-        a = arr[i]//x
-        b = arr[n-1-i]//x
-        a_ = arr[i]%x
-        b_ = arr[n-1-i]//x
-        moves += a
-        moves += b
-        remainder.append(a_)
-        remainder.append(b_)
+    for i in range(n):
+        moves += arr[i]//x
+    maxi = 0
+    for j in arr:
+        amount =  j  + (moves - (j//x))*y
+        maxi = max(maxi,amount)
         
-    ab = summed - (moves*lost)
-    if ab == 25:
-        print(ab)
-    elif ab == 229:
-        print(ab)
+    print(maxi)
--- test file.py
+++ test file.py
@@ -1,5 +1,36 @@
-a = [15,17,14,15,12,16]
-print(sum(a))
+t = int(input())
+
+for _ in range(t):
+
+    n, x, y = map(int, input().split())
+
+    arr = list(map(int, input().split()))
+
+
+
+    total_moves = 0
+
+    for val in arr:
+
+        total_moves += val // x
+
+
+
+    max_money = 0
+
+    for val in arr:
+
+        current_moves = val // x
+
+        incoming_moves = total_moves - current_moves
+
+        amount = val + (incoming_moves * y)
+
+        max_money = max(amount,max_money)
+
+
+
+    print(max_money)
 
 
 
