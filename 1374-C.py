a1e7b8793b95819c6e7ef106e3f8b12f1b6e05b7
mpssriram <msriram.0517@gmail.com>
2026-03-05 23:42:37 +0530

    --

---
+++ 1374-C.py
@@ -0,0 +1,34 @@
+"""t = int(input())
+for _ in range(t):
+    inn = int(input())
+    arr = list(input())
+    i = 0
+
+    while i < len(arr)-1:
+        if arr[i] == '(' and arr[i+1] == ')':
+            arr.pop(i)
+            arr.pop(i)
+            i = 0
+        else:
+            i += 1
+
+    
+    print(len(arr)//2)"""
+
+class Stack():
+    def __init__(self):
+        self.content = []
+
+    def push(self,x):
+        return self.content.append(x)
+
+    def pop(self):
+        self.element = self.content.pop()
+    def is_empty(self):
+        length = len(self.content)
+        if length == 0:
+            return True
+        else:
+            return False
+    def reverse_string(self,x):
+        return x[::-1]
